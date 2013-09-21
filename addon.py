528e4e2af8b0067b1ec82cf503a36b56cd0147ff
twinther <tommy@winther.nu>
2013-09-21 10:56:26 +0200

    Added tv6play.se, tv8play.se, tv10play.se and play.novatv.bg Corrected strings.xml id's (cherry picked from commit 803215d)

--- addon.py
+++ addon.py
@@ -21,7 +21,6 @@ import os
 import sys
 import urlparse
 import re
-import urllib2
 import mobileapi
 
 import xbmc
@@ -31,8 +30,8 @@ import xbmcplugin
 
 import buggalo
 
-REGIONS = ['tv3play.dk', 'tv3play.se', 'tv3play.no', 'tv3play.lt', 'tv3play.lv', 'tv3play.ee', 'viasat4play.no']
-RSS = {301: 'recent', 302: 'mostviewed', 303: 'highestrated', 304: 'recent?type=clip'}
+REGIONS = ['tv3play.dk', 'tv3play.se', 'tv3play.no', 'tv3play.lt', 'tv3play.lv', 'tv3play.ee', 'tv6play.se',
+           'tv8play.se', 'tv10play.se', 'viasat4play.no', 'play.novatv.bg']
 
 
 class TV3PlayAddon(object):
@@ -53,20 +52,10 @@ class TV3PlayAddon(object):
     def listPrograms(self, region):
         items = list()
 
-        #        # Featured
-        #        item = xbmcgui.ListItem(ADDON.getLocalizedString(305), iconImage=ICON)
-        #        item.setProperty('Fanart_Image', FANART)
-        #        items.append((PATH + '?region=%s&special=featued' % region, item, True))
-        #
-        #        # Most viewed
-        #        item = xbmcgui.ListItem(ADDON.getLocalizedString(302), iconImage=ICON)
-        #        item.setProperty('Fanart_Image', FANART)
-        #        items.append((PATH + '?region=%s&special=mostviewed' % region, item, True))
-
         formats = self.api.getAllFormats()
         if not formats:
             xbmcplugin.endOfDirectory(HANDLE, succeeded=False)
-            self.displayError('Mobile API not available for this region yet! Try later...')
+            self.displayError(ADDON.getLocalizedString(30205))
             return
 
         for series in formats:
@@ -113,6 +102,9 @@ class TV3PlayAddon(object):
                 'plotoutline': video['summary'],
                 'tvshowtitle': video['formattitle']
             }
+            if 'length' in video and video['length'] is not None:
+                infoLabels['duration'] = int(video['length']) / 60
+
             if 'airdate' in video:
                 airdate = video['airdate']
                 infoLabels['date'] = '%s.%s.%s' % (airdate[8:10], airdate[5:7], airdate[0:4])
@@ -137,26 +129,24 @@ class TV3PlayAddon(object):
         playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
         playlist.clear()
 
-        u = urllib2.urlopen('http://viastream.viasat.tv/extra/extraN.php?clipid=%s' % videoId)
-        xml = u.read()
-        u.close()
-        print xml
-        if '<Url>' in xml:
+        data = self.api.getMobileData(videoId)
+        if data is not None and 'adcalls' in data and data['adcalls'][0]['type'] == 'preroll':
+            xml = self.api._http_request(data['adcalls'][0]['url'])
 
-            m = re.search('<Url>(.*)</Url>', xml)
+            m = re.search('<MediaFile[^>]+><!\[CDATA\[(.*)\]\]></MediaFile>', xml)
             if m:
-                rtmp = m.group(1).replace('rtmp://tv3playee.data.lt/mtg/', 'rtmp://tv3playee.data.lt/mtg/mtg/')
-                rtmp = rtmp.replace('mp4:flash', 'flash')
-                rtmp = rtmp.replace(' ', '%20')
-                rtmp += ' swfUrl=http://flvplayer.viastream.viasat.tv/play/swf/player111227.swf swfVfy=true'
-                playlist.add(rtmp)
+                item = xbmcgui.ListItem(ADDON.getLocalizedString(30100), iconImage=ICON)
+                playlist.add(m.group(1), item)
+
+        url = self.api.getMobileStream(videoId)
+        playlist.add(url)
 
         xbmcplugin.setResolvedUrl(HANDLE, True, playlist[0])
 
     def displayError(self, message='n/a'):
         heading = buggalo.getRandomHeading()
-        line1 = ADDON.getLocalizedString(200)
-        line2 = ADDON.getLocalizedString(201)
+        line1 = ADDON.getLocalizedString(30200)
+        line2 = ADDON.getLocalizedString(30201)
         xbmcgui.Dialog().ok(heading, line1, line2, message)
 
 
@@ -182,10 +172,6 @@ if __name__ == '__main__':
             tv3PlayAddon.listVideos(PARAMS['region'][0], PARAMS['category'][0])
         elif 'format' in PARAMS:
             tv3PlayAddon.listCategories(PARAMS['region'][0], PARAMS['format'][0])
-        #        elif PARAMS.has_key('special') and PARAMS['special'] == 'featured':
-        #            tv3PlayAddon.listFeatured(PARAMS['region'][0])
-        #        elif PARAMS.has_key('special') and PARAMS['special'] == 'mostviewed':
-        #            tv3PlayAddon.listMostViewed(PARAMS['region'][0])
         elif 'region' in PARAMS:
             tv3PlayAddon.listPrograms(PARAMS['region'][0])
         else:
